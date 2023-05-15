932bd7daae2fded2eebba39b4fccacf1095c84cf
Florin Romulescu <romulescu.florin.dev@gmail.com>
2023-05-15 14:25:56 +0300

    Added Session class to control session tokens

---
+++ src/include/Session.hpp
@@ -0,0 +1,35 @@
+#ifndef SESSION_HPP
+#define SESSION_HPP
+
+#include <string>
+#include <memory>
+
+/*
+* Session class
+* This class is used for storing session information
+* like the session token, username, password and
+* whether the user is logged in or not.
+* This class is a singleton.
+* Use the start() method to create a new session.
+* Use the end() method to end a session.
+*/
+class Session {
+    private:
+        std::string sessionToken;
+        std::string username;
+        std::string password;
+        Session();
+    public:
+        ~Session();
+        static Session* session;
+        static Session* start();
+        static void end();
+        std::string getSessionToken();
+        std::string getUsername();
+        std::string getPassword();
+        void setSessionToken(std::string sessionToken);
+        void setUsername(std::string username);
+        void setPassword(std::string password);
+};
+
+#endif // SESSION_HPP
---
+++ src/lib/Session.cpp
@@ -0,0 +1,51 @@
+#include "../include/Session.hpp"
+
+Session* Session::session = nullptr;
+
+Session::Session() {
+    this->sessionToken = "";
+    this->username = "";
+    this->password = "";
+}
+
+Session::~Session() {}
+
+Session* Session::start() {
+    if (Session::session == nullptr) {
+        Session::session = new Session();
+    }
+    return Session::session;
+}
+
+void Session::end() {
+    if (session != nullptr) {
+        delete session;
+        session = nullptr;
+    }
+}
+
+std::string Session::getSessionToken() {
+    return this->sessionToken;
+}
+
+std::string Session::getUsername() {
+    return this->username;
+}
+
+std::string Session::getPassword() {
+    return this->password;
+}
+
+void Session::setSessionToken(std::string sessionToken) {
+    this->sessionToken = sessionToken;
+}
+
+void Session::setUsername(std::string username) {
+    this->username = username;
+}
+
+void Session::setPassword(std::string password) {
+    this->password = password;
+}
+
+// Path: src/lib/CommandFactory.cpp
