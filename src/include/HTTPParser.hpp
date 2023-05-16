01b2d8b608f839904669b411b3239511f04e5f64
Florin Romulescu <romulescu.florin.dev@gmail.com>
2023-05-16 11:03:41 +0300

    Added payload on POST

--- src/include/HTTPParser.hpp
+++ src/include/HTTPParser.hpp
@@ -2,6 +2,7 @@
 #define HTTP_PARSER_HPP
 
 #include <memory>
+#include "utils.hpp"
 #include "json.hpp"
 
 namespace http {
@@ -33,10 +34,26 @@ namespace http {
             json payload;
             std::string cookie;
             std::string session_token;
+            std::string query_params;
         public:
             HTTPParser();
             ~HTTPParser();
-            
+            HTTPParser* setMethod(std::string method);
+            HTTPParser* setAccessRoute(std::string access_route);
+            HTTPParser* setPayloadType(std::string payload_type);
+            HTTPParser* setPayload(json payload);
+            HTTPParser* setCookie(std::string cookie);
+            HTTPParser* setSessionToken(std::string session_token);
+            HTTPParser* setQueryParams(std::string query_params);
+            std::string getMethod();
+            std::string getAccessRoute();
+            std::string getPayloadType();
+            json getPayload();
+            std::string getCookie();
+            std::string getSessionToken();
+            std::string getQueryParams();
+            std::shared_ptr<std::string> toString();
+            static void append_newline(std::shared_ptr<std::string> str);
     };
 
 } // namespace http
---
+++ src/lib/HTTPParser.cpp
@@ -0,0 +1,124 @@
+#include "../include/HTTPParser.hpp"
+using namespace http;
+
+HTTPParser::HTTPParser() {
+    this->method = "";
+    this->access_route = "";
+    this->payload_type = "";
+    this->payload = json();
+    this->cookie = "";
+    this->session_token = "";
+}
+
+HTTPParser::~HTTPParser() {}
+
+HTTPParser* HTTPParser::setMethod(std::string method) {
+    this->method = method;
+    return this;
+}
+
+HTTPParser* HTTPParser::setAccessRoute(std::string access_route) {
+    this->access_route = access_route;
+    return this;
+}
+
+HTTPParser* HTTPParser::setPayloadType(std::string payload_type) {
+    this->payload_type = payload_type;
+    return this;
+}
+
+HTTPParser* HTTPParser::setPayload(json payload) {
+    this->payload = payload;
+    return this;
+}
+
+HTTPParser* HTTPParser::setCookie(std::string cookie) {
+    this->cookie = cookie;
+    return this;
+}
+
+HTTPParser* HTTPParser::setSessionToken(std::string session_token) {
+    this->session_token = session_token;
+    return this;
+}
+
+HTTPParser* HTTPParser::setQueryParams(std::string query_params) {
+    this->query_params = query_params;
+    return this;
+}
+
+std::string HTTPParser::getMethod() {
+    return this->method;
+}
+
+std::string HTTPParser::getAccessRoute() {
+    return this->access_route;
+}
+
+std::string HTTPParser::getPayloadType() {
+    return this->payload_type;
+}
+
+json HTTPParser::getPayload() {
+    return this->payload;
+}
+
+std::string HTTPParser::getCookie() {
+    return this->cookie;
+}
+
+std::string HTTPParser::getSessionToken() {
+    return this->session_token;
+}
+
+std::string HTTPParser::getQueryParams() {
+    return this->query_params;
+}
+
+void HTTPParser::append_newline(std::shared_ptr<std::string> str) {
+    str->append("\r\n");
+}
+
+std::shared_ptr<std::string> HTTPParser::toString() {
+    std::shared_ptr<std::string> output(new std::string());
+    output->append(this->method);
+    if (query_params != "") {
+        output->append("?");
+        output->append(query_params);
+    } else {
+        output->append(" ");
+    }
+    output->append(this->access_route);
+    output->append(" HTTP/1.1");
+    
+    HTTPParser::append_newline(output);
+    output->append("Host: ");
+    output->append(SERVER_IP);
+    HTTPParser::append_newline(output);
+
+    output->append("Content-Type: ");
+    output->append(this->payload_type);
+    HTTPParser::append_newline(output);
+
+    output->append("Connection: keep-alive");
+    HTTPParser::append_newline(output);
+
+    if (this->cookie != "") {
+        output->append("Cookie: ");
+        output->append(this->cookie);
+        HTTPParser::append_newline(output);
+    }
+
+    if (method == "POST") {
+        output->append("Content-Length: ");
+        output->append(std::to_string(this->payload.dump().length()));
+        HTTPParser::append_newline(output);
+        output->append(this->payload.dump());
+    } else if (method == "GET") {
+        output->append("Content-Length: 0");
+        HTTPParser::append_newline(output);
+    }
+
+    HTTPParser::append_newline(output);
+    return output;
+}
