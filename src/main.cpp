c28a85da43ecaa9c82d30a9b67058187971bb1de
Florin Romulescu <romulescu.florin.dev@gmail.com>
2023-05-20 12:51:38 +0300

    Am rezovat problema cu reconectarea la server

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,19 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 
+void reconnect(int& sockdfd, std::vector<pollfd>& pollfds) {
+     // Timeout
+    #ifdef DEBUG
+    std::cout << "Server disconnected" << std::endl;
+    #endif
+    // Reconnect to server
+    utils::disconnect(sockdfd);
+    sockdfd = utils::connect(SERVER_IP, SERVER_PORT, AF_INET, SOCK_STREAM, 0);
+    pollfds[1].fd = sockdfd;
+    #ifdef DEBUG
+    std::cout << "Reconnected to server" << std::endl;
+    #endif
+}
 
 using json = nlohmann::json;
 
@@ -59,6 +72,8 @@ int main() {
     // This variable is used for prompt printing
     bool command_running = true;
 
+    #define TIMEOUT 4000
+
     do {
         // Print prompt
         if (command_running) {
@@ -73,7 +88,19 @@ int main() {
             std::cout << "\033[0m" << "]> ";
             std::cout.flush();
         }
-        poll(pollfds.data(), pollfds.size(), -1);
+        int ret = poll(pollfds.data(), pollfds.size(), TIMEOUT);
+        if (ret == 0) {
+            // Timeout
+            reconnect(sockdfd, pollfds);
+            command_running = false;
+            continue;
+        } else if (ret == -1) {
+            // Error
+            #ifdef DEBUG
+            std::cout << "Error" << std::endl;
+            #endif
+            continue;
+        }
         for (int i = 0; i < (int)pollfds.size(); i++) {
             if (pollfds[i].revents & POLLIN) {
                 if (pollfds[i].fd == 0) {
@@ -82,16 +109,7 @@ int main() {
                 } else {
                     std::string response = utils::receive(sockdfd);
                     if (response == "") {
-                        #ifdef DEBUG
-                        std::cout << "Server disconnected" << std::endl;
-                        #endif
-                        // Reconnect to server
-                        utils::disconnect(sockdfd);
-                        sockdfd = utils::connect(SERVER_IP, SERVER_PORT, AF_INET, SOCK_STREAM, 0);
-                        pollfds[1].fd = sockdfd;
-                        #ifdef DEBUG
-                        std::cout << "Reconnected to server" << std::endl;
-                        #endif
+                        reconnect(sockdfd, pollfds);
                         command_running = false;
                         continue;
                     }
@@ -103,6 +121,9 @@ int main() {
                 }
             }
         }
+        // Reconnect to server
+        reconnect(sockdfd, pollfds);
+        // Send requests
         while (!Session::session->requests->empty()) {
             std::shared_ptr<std::string> request = Session::session->requests->front();
             Session::session->requests->pop();
